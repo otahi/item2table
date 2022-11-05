60d958dcfb3f750e0110acf240ee8c14f8a95cce
Hiroshi Ota <otahi.pub@gmail.com>
2022-11-06 07:47:00 +0900

    Refactor

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function createNodeList() {
     const text = document.getElementById('items').value;
     const lines = text.split('\n');
 
-    const nodes = { indent: -1, parent: null, children: [], row: -1, col: -1, rows: 0, depth: 0 };
+    const nodeList = { indent: -1, parent: null, children: [], row: -1, col: -1, rows: 0, depth: 0 };
     let lastNode = null;
     let lastIndent = 0;
     let lastRow = -1;
@@ -39,7 +39,7 @@ function createNodeList() {
 
     function findParentNode(lastNode, indent) {
         if (lastNode == null) {
-            return nodes;
+            return nodeList;
         } else if (lastNode.indent < indent) {
             return lastNode;
         } else {
@@ -55,26 +55,39 @@ function createNodeList() {
 
         const indent = countIndent(line);
         const parent = findParentNode(lastNode, indent);
-        const row = lastIndent >= indent ? lastRow + 1 : lastRow;
-        const col = lastIndent >= indent ? parent.col + 1 : lastColumn + 1;
-        const node = { body: textBody(line), indent: indent, parent: parent, children: [], row: row, col: col };
 
+        let row, col = (0, 0);
+        if (lastIndent >= indent) {
+            // go next row and get back to parent's children col
+            row = lastRow + 1;
+            col = parent.col + 1;
+        } else {
+            // keep row and go deeper col
+            row = lastRow;
+            col = lastColumn + 1;
+        }
+
+        const node = { body: textBody(line), indent: indent, parent: parent, children: [], row: row, col: col };
         parent.children.push(node);
 
+        // save last status
         lastIndent = indent;
         lastRow = row;
         lastColumn = col;
         lastNode = node;
+
+        // keep max row and depth
         maxRow = maxRow > row ? maxRow : row;
         maxDepth = maxDepth > col ? maxDepth : col;
     }
 
-    nodes.depth = maxDepth + 1;
-    nodes.rows = maxRow + 1;
+    nodeList.depth = maxDepth + 1;
+    nodeList.rows = maxRow + 1;
 
-    return nodes;
+    return nodeList;
 }
 
+// Print Node for debug
 function printNode(nodeList) {
     if (nodeList.body) {
         console.log(`${nodeList.body}, row=${nodeList.row}, col=${nodeList.col}`);
@@ -103,30 +116,12 @@ function getNumChildren(node) {
     return sum + 1;
 }
 
-function getDepth(nodeList) {
-    let maxDepth = 0;
-    const children = nodeList.children;
-
-    if (children == null || children.length == 0) {
-        return 1;
-    }
-
-    for (const child of children) {
-        const depth = getDepth(child);
-        maxDepth = depth > maxDepth ? depth : maxDepth;
-    }
-
-    return maxDepth + 1;
-}
-
-function fillChildren(cells, children) {
-    for (const node of children) {
+function convertNodesToCells(cells, nodes) {
+    for (const node of nodes) {
         cells[node.row][node.col] = node.body;
-        fillChildren(cells, node.children);
+        convertNodesToCells(cells, node.children);
     }
 }
-
-
 
 function getRowspan(row, col, cells) {
     let span = 0;
@@ -151,6 +146,7 @@ function getRowspan(row, col, cells) {
 function getColspan(row, col, cells) {
     let span = 0;
     for (let j = col + 1; j < cells[row].length; j++) {
+        // increment while deeper col is empty
         if (cells[row][j]) {
             break;
         }
@@ -169,7 +165,7 @@ function createTable(nodeList) {
         cells[i] = new Array(columnLength).fill(null);
     }
 
-    fillChildren(cells, nodeList.children);
+    convertNodesToCells(cells, nodeList.children);
 
     let html = '<tbody><tr>';
     for (let i = 0; i < columnLength; i++) {
@@ -178,22 +174,21 @@ function createTable(nodeList) {
     html += '</tr><tr>';
     for (let i = 0; i < rowLength; i++) {
         for (let j = 0; j < columnLength; j++) {
-            const rowspan = getRowspan(i, j, cells);
-            const colspan = getColspan(i, j, cells);
-            if (rowspan > 1 && cells[i][j]) {
-                html += `<td rowspan="${rowspan}">${cells[i][j]}</td>`;
-            } else if (colspan > 1 && cells[i][j]) {
-                html += `<td colspan="${colspan}">${cells[i][j]}</td>`;
-                break;
-            } else {
-                if (cells[i][j]) {
+            if (cells[i][j]) {
+                const rowspan = getRowspan(i, j, cells);
+                const colspan = getColspan(i, j, cells);
+                if (rowspan > 1) {
+                    html += `<td rowspan="${rowspan}">${cells[i][j]}</td>`;
+                } else if (colspan > 1) {
+                    html += `<td colspan="${colspan}">${cells[i][j]}</td>`;
+                    break;
+                } else {
                     html += `<td>${cells[i][j]}</td>`;
                 }
             }
         }
         html += '</tr>';
     }
-
 
     html += '</tbody>';
     const table = document.getElementById('table');
